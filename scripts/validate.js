581aa97c102cf77970c687491bac6e8292b457c8
elicrock <rockelic@yandex.ru>
2023-03-16 01:45:39 +0300

    Sprint 6 - validation continue

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,3 +1,45 @@
+const showInputError = (inputElement, errorElement, validationMessage, inputErrorClass, errorClass)  => {
+  inputElement.classList.add(inputErrorClass);
+  errorElement.textContent = validationMessage;
+  errorElement.classList.add(errorClass);
+};
+
+const hideInputError = (inputElement, errorElement, inputErrorClass, errorClass) => {
+  inputElement.classList.remove(inputErrorClass);
+  errorElement.textContent = '';
+  errorElement.classList.remove(errorClass);
+};
+
+const checkInputValidity = (inputElement, inputErrorClass, errorClass) => {
+  const errorElement = document.querySelector(`.${inputElement.id}-error`);
+  if (!inputElement.validity.valid) {
+    showInputError(inputElement, errorElement, inputElement.validationMessage, inputErrorClass, errorClass);
+  } else {
+    hideInputError(inputElement, errorElement, inputErrorClass, errorClass);
+  }
+};
+
+const setEventListeners = (formList, inputList, inputErrorClass, errorClass) => {
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', (event) => {
+      event.preventDefault();
+    });
+  });
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', (event) => {
+      checkInputValidity(inputElement, inputErrorClass, errorClass);
+    });
+  });
+};
+
+const enableValidation = (config) => {
+  const formList = Array.from(document.querySelectorAll(config.formSelector));
+  const inputList = Array.from(document.querySelectorAll(config.inputSelector));
+
+  setEventListeners(formList, inputList, config.inputErrorClass, config.errorClass);
+};
+
 enableValidation({
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
